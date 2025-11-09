29c1022399a67fe19e4daa8610f185de8006efc7
AnnMargaret Tutu <codeamt@users.noreply.github.com>
2025-11-09 01:04:15 -0500

    scaffolded add-on module structure

--- app/core/routes/graphql.py
+++ app/core/routes/graphql.py
@@ -2,6 +2,24 @@ from fastapi import APIRouter
 from strawberry.fastapi import GraphQLRouter
 from app.core.schemas.graphql.schema import schema
 
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    async def recommend_products(self, prefs: List[str]) -> List[Product]:
+        return await Recommender.recommend_products(prefs)
+
+    @strawberry.field
+    async def recommend_courses(self, interests: List[str]) -> List[Course]:
+        return await Recommender.recommend_courses(interests)
+
+    @strawberry.field
+    async def hybrid_recommendation(self, user_id: str) -> Recommendation:
+        return await Recommender.hybrid_recommendation(user_id)
+
+schema = strawberry.Schema(Query)
+
+
 router_graphql = APIRouter()
 graphql_app = GraphQLRouter(schema)
 router_graphql.include_router(graphql_app, prefix="/graphql")
---
+++ app/settings.py
@@ -0,0 +1,5 @@
+# App Settings 
+
+from pydantic_settings import BaseSettings
+class Settings(BaseSettings):
+    ...
